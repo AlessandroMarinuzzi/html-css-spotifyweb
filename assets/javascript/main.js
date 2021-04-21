a78a2310f83473e3f32e2ac6277b322bc79199b4
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-04-21 14:35:13 +0100

    Final Commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,6 @@
+// writeln
+document.writeln('What s the meaning of this line?');
+// alert
+alert('Let s travel together through the boolean course');
+// getElementby
+document.getElementById('stranger').innerHTML = "Stranger things is overrated.";
